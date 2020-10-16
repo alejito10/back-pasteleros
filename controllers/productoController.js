531b282a116e24c68aa94e4363e75f42223c3d29
Alejandro González <alejandre.gp@hotmail.com>
2020-10-16 11:21:34 +0200

    productosController y crudProductos añadido

---
+++ controllers/productoController.js
@@ -0,0 +1,17 @@
+const crud = require('../lib/crudProductos');
+
+const newProducto = async (req, res) =>{
+    try{
+        const newProducto = await crud.createNewProducto(req, res);
+        console.log(newProducto);
+
+        res.status(200).json({status:200, message:"insertado correctamente", data:newProducto});
+    }
+    catch(err){
+        res.status(401).json({status:401,message:err.message,data:[]});
+    }
+};
+
+module.exports = {
+    newProducto,
+};
