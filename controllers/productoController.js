58bf88a7131e1d4a56563934584cef15e060ace4
Alejandro González <alejandre.gp@hotmail.com>
2020-10-17 18:26:38 +0200

    modificacion de producto y encotrar todos los productos

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -12,6 +12,52 @@ const newProducto = async (req, res) =>{
     }
 };
 
+//buscar un producto por su nombre
+const findOneProducto = async (req,res,err) =>{
+    try{
+        if(!req.query.hasOwnProperty('name')||req.query.name == ""){
+            res.status(200).json({status:400,message:"falta en el query name", data:[]});
+        }
+        const producto = await crud.findOneProducto(req.query.name);
+        if(Object.keys(producto).length==0) res.status(200).json({status:404,message:"NOT FOUND name",data:[]});
+
+        console.log(producto);
+        res.status(200).json({status:200,message:"leido correctamente",data:producto});
+    }
+    catch(err){
+        res.status(404).json({status:404,message:err.message,data:producto});
+    }
+};
+
+//
+const findAllProductos = async(req,res)=>{
+    try{
+        const productos = await crud.findAllProductos(req,res);
+        if(Object.keys(productos).length==0) res.status(200).json({status:404, message:"no existen productos",data:[]});
+        res.status(200).json({status:200,message:"leido correctamente", data:productos});
+    }
+    catch(err){
+        res.status(403).json({status:403,message:err.message,data:[]});
+    }
+};
+
+
+const updateOneProducto = async (req, res, next) => {
+	try {
+		const producto = await crud.updateOneProducto(req);
+		if (Object.keys(producto).length == 0)
+			res
+				.status(200)
+				.json({ status: 404, message: "NOT FOUND name", data: [] });
+		res
+			.status(200)
+			.json({ status: 200, message: "modificado correctamente", data: producto });
+	} catch (err) {
+		res.status(401).json({ status: 401, message: err.message, data: [] });
+	}
+};
 module.exports = {
-    newProducto
+    newProducto,
+    findOneProducto,
+    findAllProductos,
 };
--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -2,8 +2,18 @@ var express = require("express");
 var router = express.Router();
 
 const productosController = require('../controllers/productoController');
+const productoController = require("../controllers/productoController");
 
 router.route('/new')
     .post(productosController.newProducto);
+
+router.route('/')
+    .get(productosController.findOneProducto)
+    .get(productoController.findAllProductos);
+    
+router.route("/todos")
+    .get(productoController.findAllProductos);
+
+
 
 module.exports = router;
