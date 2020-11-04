f374089a5a0883af35d634b294325b96cc488320
Alejandro González <alejandre.gp@hotmail.com>
2020-11-04 19:58:53 +0100

    mejoras en los crud

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,9 @@ var upload = multer({ storage: storage });
 const userController = require('../controllers/userController')
 const imgController = require('../controllers/cloudinaryController')
 
+
+router.route('/new').post(userController.newUser);
+
 router.route('/imagen')
     .post(upload.single('image'), imgController, userController.addImgUser);
 module.exports = router;
